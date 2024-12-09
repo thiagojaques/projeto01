914ca9b12a9620237f4eacbd675aba97dd4e292b
Thiago <thiagohenriquedelimajaques@gmail.com>
2024-12-09 12:14:10 -0400

    Adicionado testes ao formulário
    Close #24

--- portfolio/base/tests.py
+++ portfolio/base/tests.py
@@ -1,3 +1,17 @@
-def test_home_status_code(client):
-    resposta = client.get('/')
+from django.urls import reverse
+from pytest_django.asserts import assertContains
+import pytest
+
+@pytest.fixture
+def resposta(client):
+    resp = client.get(reverse('base:home'))
+    return resp
+
+def test_status_code(resposta):
     assert resposta.status_code == 200
+
+def test_formulario_presente(resposta):
+    assertContains(resposta, '<form')
+
+def test_do_botao_salvar_presente(resposta):
+    assertContains(resposta, '<button type="submit"')
--- portfolio/settings.py
+++ portfolio/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'portfolio.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates', 'statics'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,7 +116,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'statics/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
