b5f3d383cd535680d5045a0b608aba011def5af0
Thiago <thiagohenriquedelimajaques@gmail.com>
2024-12-19 11:01:46 -0400

    Configurado o setup inicial para adicionar o ecommerce básico.
    Close #32

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -16,9 +16,19 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('portfolio.base.urls')),
-]
 
+    
+] + static(settings.MEDIA_URL, ducument_root=settings.MEDIA_ROOT)
+
+# TODO : Remover debug toolbar
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
