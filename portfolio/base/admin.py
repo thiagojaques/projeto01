235b4d8a330b8c6334eafa0d4ca9ba4cecf6f844
Thiago <thiagohenriquedelimajaques@gmail.com>
2024-12-23 09:59:48 -0400

    Resolvido o bug de banco de dados
    Close #34

--- portfolio/base/admin.py
+++ portfolio/base/admin.py
@@ -1,2 +1,7 @@
+# base/admin.py
+from django.contrib import admin
+from .models import Contato
 
-# Register your models here.
+@admin.register(Contato)
+class ContatoAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'email', 'mensagem')
